b09b763f2c4f488fc65d37541f65ff54a1402559
Espen Henriksen <dev@esphen.com>
2015-03-26 22:33:09 +0100

    Initial commit

---
+++ webworker.js
@@ -0,0 +1,35 @@
+angular.module('web-worker', [])
+    .factory('WebWorkers', function ($q) {
+        'use strict';
+        return {
+            run: function (fn, params) {
+                var deferred = $q.defer();
+                // Strip wrapper function
+                var entire = fn.toString();
+                var functionBody = entire.slice(entire.indexOf('function'));
+                functionBody = 'onmessage =' + functionBody;
+
+                // URL.createObjectURL
+                window.URL = window.URL || window.webkitURL;
+
+                var blob;
+                try {
+                    blob = new Blob([functionBody], {type: 'application/javascript'});
+                } catch (e) { // Backwards-compatibility
+                    window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+                    blob = new BlobBuilder();
+                    blob.append(functionBody);
+                    blob = blob.getBlob();
+                }
+                var worker = new Worker(URL.createObjectURL(blob));
+
+                // Processed data returns
+                worker.onmessage = function(e) {
+                    deferred.resolve(e.data);
+                };
+                // Start worker
+                worker.postMessage(params);
+                return deferred.promise;
+            }
+        };
+    });
